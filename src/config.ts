c8eec0005e4ceccc0cc9253d8cbe6435e2047419
NAI - VIIjeer <wallesalex205@gmail.com>
2026-09-08 11:30:06 +0300

    fix(config): update API_BASE_URL constant

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = import.meta.env.BASE_API_URL;
+export const API_BASE_URL = import.meta.env.API_BASE_URL;
 export const GOOGLE_CLIENT_ID = import.meta.env.GOOGLE_CLIENT_ID;
